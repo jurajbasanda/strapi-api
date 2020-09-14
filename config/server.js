7b9a11b6ad753dc633cf7c749eac84ee4a5ed095
Juraj Basanda <basanda.juraj@gmail.com>
2020-09-14 15:30:32 +0100

    commit

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '23745f9ee876508c1a58bede475531e1'),
