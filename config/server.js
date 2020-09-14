2f37845e4e1823cd2be54f50a8bf474dec5b4bc4
Juraj Basanda <basanda.juraj@gmail.com>
2020-09-14 15:04:38 +0100

    commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '23745f9ee876508c1a58bede475531e1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a85bfffe-1ae5-41c9-865a-2ebdac3d3717'
+};
